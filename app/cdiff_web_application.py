964d2bded83e75ac64d8bc703c77118d30e0fae9
Walter <swsingaram@gmail.com>
2019-09-26 10:18:03 -0400

    modified:   app/cdiff_web_application.py 	modified:   mimiciii_scratch_work.ipynb

--- app/cdiff_web_application.py
+++ app/cdiff_web_application.py
@@ -18,49 +18,73 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 #app = dash.Dash()
 
 app.layout = html.Div([
-    html.Label('Admission Type'),
-    dcc.Dropdown(
-        id='adm_type',
-        options=[
-            {'label': 'Select one', 'value': 'SO'},
-            {'label': 'Elective', 'value': 'ELECTIVE'},
-            {'label': 'Emergency', 'value': 'EMERGENCY'},
-            {'label': 'Newborn', 'value': 'NEWBORN'},
-            {'label': 'Urgent', 'value': 'URGENT'}
-        ],
-        value='SO'
-    ),
+    html.H1(children='CDiffense'),
 
-    html.Label('Admission Location'),
-    dcc.Dropdown(
-        id='adm_loc',
-        options=[
-            {'label': 'Select one', 'value': 'SO'},
-            {'label': 'Clinic Referral/Premature', 'value': 'CLINIC REFERRAL/PREMATURE'},
-            {'label': 'Emergency Room Admit', 'value': 'EMERGENCY ROOM ADMIT'},
-            {'label': 'HMO Referral', 'value': 'HMO REFERRAL/SICK'},
-            {'label': 'Physician Referral', 'value': 'PHYS REFERRAL/NORMAL DELI'},
-            {'label': 'Transfer from hospital', 'value':'TRANSFER FROM HOSP/EXTRAM'},
-            {'label': 'Transfer from other healcare provider', 'value':'TRANSFER FROM OTHER HEALT'},
-            {'label': 'Transfer from skilled nurse', 'value':'TRANSFER FROM SKILLED NUR'},
-            {'label': 'Transfer within this facility', 'value':'TRSF WITHIN THIS FACILITY'}
-        ],
-        value='SO'
-    ),
+    html.H6(children=dcc.Markdown('''
+ A clinical decision resource to assess patient risk for Clostridium Difficile (*C. Diff.*)
+   ''')),
 
-    html.Label('Ethnicity'),
-    dcc.Dropdown(
-        id='race',
-        options=[
-            {'label': 'Select one', 'value': 'SO'},
-            {'label': 'Black', 'value': 'BLACK/AFRICAN AMERICAN'},
-            {'label': 'White', 'value': 'WHITE'},
-            {'label': 'Hispanic or Latino', 'value': 'HISPANIC OR LATINO'},
-            {'label': 'Asian', 'value': 'ASIAN'},
-            {'label': 'Other', 'value': 'OTHER'}
-        ],
-        value='SO'
-    ),
+    html.Div('',style={'padding':10}),
+
+    html.Div([
+        html.Label('Admission Type',style={'font-weight':'bold'}),
+        dcc.Dropdown(
+            id='adm_type',
+            options=[
+                {'label': 'Select one', 'value': 'SO'},
+                {'label': 'Elective', 'value': 'ELECTIVE'},
+                {'label': 'Emergency', 'value': 'EMERGENCY'},
+                {'label': 'Newborn', 'value': 'NEWBORN'},
+                {'label': 'Urgent', 'value': 'URGENT'}
+            ],
+            value='SO'
+        ,style={'columnCount':1}),
+      
+        html.Label('Admission Location'),
+        dcc.Dropdown(
+            id='adm_loc',
+            options=[
+                {'label': 'Select one', 'value': 'SO'},
+                {'label': 'Clinic Referral/Premature', 'value': 'CLINIC REFERRAL/PREMATURE'},
+                {'label': 'Emergency Room Admit', 'value': 'EMERGENCY ROOM ADMIT'},
+                {'label': 'HMO Referral', 'value': 'HMO REFERRAL/SICK'},
+                {'label': 'Physician Referral', 'value': 'PHYS REFERRAL/NORMAL DELI'},
+                {'label': 'Transfer from hospital', 'value':'TRANSFER FROM HOSP/EXTRAM'},
+                {'label': 'Transfer from other healcare provider', 'value':'TRANSFER FROM OTHER HEALT'},
+                {'label': 'Transfer from skilled nurse', 'value':'TRANSFER FROM SKILLED NUR'},
+                {'label': 'Transfer within this facility', 'value':'TRSF WITHIN THIS FACILITY'}
+            ],
+            value='SO'
+        ,style={'columnCount':1}),
+        html.Label('Ethnicity'),
+        dcc.Dropdown(
+            id='race',
+            options=[
+                 {'label': 'Select one', 'value': 'SO'},
+                 {'label': 'Black', 'value': 'BLACK/AFRICAN AMERICAN'},
+                {'label': 'White', 'value': 'WHITE'},
+                {'label': 'Hispanic or Latino', 'value': 'HISPANIC OR LATINO'},
+                {'label': 'Asian', 'value': 'ASIAN'},
+                {'label': 'Other', 'value': 'OTHER'}
+            ],
+            value='SO'
+        ,style={'columnCount':1})
+
+   ],style={'columnCount':3}),
+
+    html.Div('',style={'padding':10}),
+    html.Div([
+    html.Label('Age:'),
+    dcc.Input(id='age',placeholder='enter age',type='text',style={'columnCount':1}),
+
+    html.Label('Body Temperature (F):'),
+    dcc.Input(id='temperature',placeholder='enter temperature',type='text',style={'columnCount':1}),
+
+    html.Label('Heart Rate (beats per min):'),
+    dcc.Input(id='heart_rate',placeholder='enter heart rate',type='text',style={'columnCount':1})
+    ],style={'columnCount':3}),
+
+    html.Div('',style={'padding':10}),
 
     html.Label('Preliminary Diagnosis'),
     dcc.Checklist(
@@ -79,8 +103,9 @@ app.layout = html.Div([
             {'label': 'Diarrhea', 'value':'DIARRHEA'}
         ],
         value=[]
-    ),
+    ,style={'columnCount':2}),
 
+    html.Div('',style={'padding':10}),
 
     html.Label('Estimated Length of Stay (Days)'),
     dcc.Slider(
@@ -91,10 +116,12 @@ app.layout = html.Div([
         value=0,
     ),
 
-    html.Label('OUTCOME'),
+    html.H3('',style={'padding':10}),
+    html.H3('Risk Assessment'),
     html.Div(id='output'),
 
-], style={'columnCount': 1})
+],style={'width': '850px', 'margin-right': 'auto', 'margin-left': 'auto'})
+#columnCount': 1})
 
 @app.callback(
     Output(component_id='output',component_property='children'),
@@ -104,7 +131,9 @@ app.layout = html.Div([
      Input('symptoms', 'value'),
      Input('los', 'value')])
 def update_outcome(adm_typ,adm_loc,race,symptoms,los):
-    model = joblib.load("../models/logistic_reg.mdl")
+    #model = joblib.load("../models/logistic_reg.mdl")
+    #df = pd.read_csv("../data/df_for_app.csv")
+    model = joblib.load("../models/logistic_regression_auc_071.mdl")
     df = pd.read_csv("../data/df_for_app.csv")
   
     labels = []
@@ -127,14 +156,14 @@ def update_outcome(adm_typ,adm_loc,race,symptoms,los):
         for l in symptoms:
             labels.append(l)
             values.append(1)
-       
+    ###### TRY THRESHOLD 0.7   
     labels.append("LOS")
     values.append(los)
     if symptoms != []:
         #print(dict(zip(labels,values)))
         df = df.append(dict(zip(labels,values)),ignore_index=True)
         df.iloc[:,:].fillna(0,inplace=True)
-        risk_value = model.predict(df.iloc[-1:])
+        risk_value = model.predict_proba(df.iloc[-1:])[:,1]
         return risk_value
     if symptoms == []:
         return "LOW RISK"
